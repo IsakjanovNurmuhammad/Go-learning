bfbc60165f0d808c3d7128ccca46000ff071f8aa
unknown <andro3star@gamil.com>
2023-08-23 14:48:38 +0500

    23/08

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+// Q2.1
+//
+//	func main() {
+//		for i := 10; i < 10; i++ {
+//			fmt.Printf("%d\n", i)
+//		}
+//	}
+//
+
+// Q2.2
+//func main() {
+//	num := 0
+//here:
+//	fmt.Printf("%d\n", num)
+//	if num < 10 {
+//		num++
+//		goto here
+//	}
+//}
+
+// Q3
+//func main() {
+//	const (
+//		FIZZ = 3
+//		BUZZ = 5
+//	)
+//	for i := 1; i < 100; i++ {
+//		if i%FIZZ == 0 {
+//			fmt.Print("Fizz")
+//		}
+//		if i%BUZZ == 0 {
+//			fmt.Print("Buzz")
+//		}
+//		fmt.Println()
+//	}
+//}
